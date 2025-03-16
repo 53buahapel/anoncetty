9db50981e5890fcf8e06c137b79646137bc6ff71
Rafi Nur Ardiansyah <rafimaster9@gmail.com>
2025-03-16 11:47:58 +0700

    feat: implement header and loading skeleton for posts page

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -4,22 +4,15 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Label } from "@/components/ui/label";
+import Header from "@/components/Header";
 
 export default function CreatePost() {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="relative py-4 shadow-sm bg-[#F4E5DA] text-[#144949]">
-        <h1 className="text-3xl font-bold text-center">AnonCetty</h1>
-
-        <img
-          src="/Indomie_logo.png"
-          alt="AnonCetty"
-          className="absolute right-4 top-1/2 -translate-y-1/2 w-10 h-10"
-        />
-      </header>
+      <Header />
 
       <main className="flex-1 flex items-center justify-center px-4 ">
-        <div className="w-full max-w-md rounded-md p-6 border-2 border-[#144949] shadow-md">
+        <div className="w-full max-w-md rounded-md p-6 border-2 border-[#144949] shadow-md bg-[#F4E5DA]">
           <h2 className="mb-4 text-xl font-semibold text-center">
             Buat Thread Kamu Disini!
           </h2>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
-import Image from "next/image";
+"use client";
+
+import { Suspense, useEffect, useState } from "react";
 import Link from "next/link";
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+import PostsSkeleton from "@/components/PostsSkeleton";
 
 export default function Home() {
+  const [fetchThread, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 800);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <h1>test</h1>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+
+      {/* Create Post Button */}
+      <div className="flex justify-center mt-6">
+        <Link href="/create">
+          <Button className="bg-primary text-primary-foreground px-6 py-2 hover:bg-primary/90">
+            Create Post
+          </Button>
+        </Link>
+      </div>
+      
+      {/* Posts */}
+      <div className="grid grid-cols-3 gap-6 p-6">
+        {Array.from({ length: 6 }).map((_, i) => (
+          <div
+            key={i}
+            className="h-40 bg-card text-card-foreground border border-border rounded-md flex items-center justify-center text-lg font-semibold shadow transition-all duration-500">
+            {fetchThread ? (
+              <PostsSkeleton /> // Single rectangle skeleton
+            ) : (
+              <p>Post {i + 1}</p> // Final content
+            )}
+          </div>
+        ))}
+      </div>
+
+
+    </div>
   );
 }
